6c03c92e6f45baeb0506b4fbfe8ac258d14d6fcf
unknown <shota.pertakhia.1@iliauni.edu.ge>
2021-06-26 23:23:00 +0400

    create token logic

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -10,9 +11,16 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
+
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
