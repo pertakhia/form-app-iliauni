985ad23afb8789269e6515df5b27f6c2b5cff24a
dunkman32 <dunkman032@gmail.com>
2021-07-12 19:39:24 +0400

    added form

---
+++ frontend/src/constants/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        question: 'ხომ არ დაგიდასტურდათ კოვიდ-19?',
+        no: 2,
+        yes: 3
+    },
+    {
+        id: 2,
+        question: 'გინდათ ტესტის გაგრძელება?',
+        no: 6,
+        yes: 1
+    },
+    {
+        id: 3,
+        question: 'ახლაც ხო არ გჭირს?',
+        no: 4,
+        yes: 5
+    },
+    {
+        id: 4,
+        question: 'დაგიტოვა სიმპტომი?',
+        no: 6,
+        yes: 6
+    },
+    {
+        id: 5,
+        question: 'თვით იზოლაციაში ხარ?',
+        no: 6,
+        yes: 6
+    },
+    {
+        id: 6,
+        question: 'თავს მიხედე',
+        no: null,
+        yes: null
+    }
+]
